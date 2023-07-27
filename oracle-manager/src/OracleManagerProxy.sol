4dbcef2c37497fca532d46205d36b6568b5f2626
Artur Begyan <arturbegyan98@gmail.com>
2023-07-27 22:26:34 +0400

    feat: introduce oracle manager proxy

--- oracle-manager/src/OracleManagerProxy.sol
+++ oracle-manager/src/OracleManagerProxy.sol
@@ -6,3 +6,13 @@ You may obtain a copy of the License at
 https://github.com/Voltz-Protocol/v2-core/blob/main/products/dated-irs/LICENSE
 */
 pragma solidity >=0.8.19;
+
+import "@voltz-protocol/util-contracts/src/proxy/UUPSProxyWithOwner.sol";
+
+/**
+ * Voltz V2 Oracle Manager Proxy
+ */
+contract OracleManagerProxy is UUPSProxyWithOwner {
+    // solhint-disable-next-line no-empty-blocks
+    constructor(address firstImplementation, address initialOwner) UUPSProxyWithOwner(firstImplementation, initialOwner) { }
+}
