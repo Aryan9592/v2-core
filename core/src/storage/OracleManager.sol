a418aa74418a93ac5fec5b7065e6244852d5ad02
Artur Begyan <arturbegyan98@gmail.com>
2023-07-27 17:20:29 +0400

    feat: introduce an oracle manager

---
+++ core/src/storage/OracleManager.sol
@@ -0,0 +1,33 @@
+/*
+Licensed under the Voltz v2 License (the "License"); you
+may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://github.com/Voltz-Protocol/v2-core/blob/main/core/LICENSE
+*/
+pragma solidity >=0.8.19;
+
+/**
+ * @title Represents Oracle Manager
+ */
+library OracleManager {
+    bytes32 private constant _SLOT_ORACLE_MANAGER =
+    keccak256(abi.encode("xyz.voltz.OracleManager"));
+
+    struct Data {
+        /**
+         * @dev The oracle manager address.
+         */
+        address oracleManagerAddress;
+    }
+
+    /**
+     * @dev Loads the singleton storage info about the oracle manager.
+     */
+    function load() internal pure returns (Data storage oracleManager) {
+        bytes32 s = _SLOT_ORACLE_MANAGER;
+        assembly {
+            oracleManager.slot := s
+        }
+    }
+}
---
+++ core/test/storage/CollateralPool.t.sol
@@ -0,0 +1,17 @@
+/*
+Licensed under the Voltz v2 License (the "License"); you
+may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://github.com/Voltz-Protocol/v2-core/blob/main/core/LICENSE
+*/
+pragma solidity >=0.8.19;
+
+import "forge-std/Test.sol";
+import "../../src/storage/CollateralPool.sol";
+
+// todo: implement tests using the branching tree technique
+
+contract CollateralPoolTest is Test {
+
+}
