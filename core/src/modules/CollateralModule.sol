0f129f98532eb46c54d17bce59018f63ee92bc11
tudor47 <tudorcostin7@gmail.com>
2023-08-01 19:39:59 +0300

    feat: sort the events in deposit function

--- core/src/modules/CollateralModule.sol
+++ core/src/modules/CollateralModule.sol
@@ -36,45 +36,60 @@ contract CollateralModule is ICollateralModule {
      */
     function deposit(uint128 accountId, address collateralType, uint256 tokenAmount) external override {
         FeatureFlag.ensureAccessToFeature(_GLOBAL_FEATURE_FLAG);
+
+        // check if collateral is enabled
         CollateralConfiguration.collateralEnabled(collateralType);
+
+        // grab the account and check its existance
         Account.Data storage account = Account.exists(accountId);
-        CollateralPool.Data storage collateralPool = CollateralPool.exists(account.trustlessProductIdTrustedByAccount);
+
         address depositFrom = msg.sender;
         address self = address(this);
 
-        uint256 actualTokenAmount = tokenAmount;
-
+        // if liquidation booster is not full, add the delta to the deposited collateral amount
         uint256 liquidationBooster = CollateralConfiguration.load(collateralType).liquidationBooster;
-        if (account.collaterals[collateralType].liquidationBoosterBalance < liquidationBooster) {
-            uint256 liquidationBoosterTopUp =
-                liquidationBooster - account.collaterals[collateralType].liquidationBoosterBalance;
-            actualTokenAmount += liquidationBoosterTopUp;
-            // todo: note, it looks like the increaseLiquidationBoosterBalance happens before the transfer,
-            // is this safe? (CR)
-            account.collaterals[collateralType].increaseLiquidationBoosterBalance(liquidationBoosterTopUp);
-            collateralPool.increaseCollateralBalance(collateralType, liquidationBoosterTopUp);
-            emit Collateral.LiquidatorBoosterUpdate(
-                accountId, collateralType, liquidationBoosterTopUp.toInt(), block.timestamp
+        uint256 liquidationBoosterBalance = account.collaterals[collateralType].liquidationBoosterBalance;
+
+        uint256 liquidationBoosterTopUp =
+            (liquidationBooster > liquidationBoosterBalance) ? liquidationBooster - liquidationBoosterBalance : 0;
+
+        uint256 actualTokenAmount = tokenAmount + liquidationBoosterTopUp;
+
+        // check that this deposit does not reach the cap
+        uint256 currentBalance = IERC20(collateralType).balanceOf(self);
+        uint256 collateralCap = CollateralConfiguration.load(collateralType).cap;
+        if (collateralCap < currentBalance + actualTokenAmount) {
+            revert CollateralCapExceeded(
+                collateralType, collateralCap, currentBalance, tokenAmount, liquidationBoosterTopUp
             );
         }
 
+        // check allowance
         uint256 allowance = IERC20(collateralType).allowance(depositFrom, self);
         if (allowance < actualTokenAmount) {
             revert IERC20.InsufficientAllowance(actualTokenAmount, allowance);
         }
 
-        uint256 currentBalance = IERC20(collateralType).balanceOf(self);
-        uint256 collateralCap = CollateralConfiguration.load(collateralType).cap;
-        if (collateralCap < currentBalance + actualTokenAmount) {
-            revert CollateralCapExceeded(
-                collateralType, collateralCap, currentBalance, tokenAmount, actualTokenAmount - tokenAmount
+        // execute transfer
+        collateralType.safeTransferFrom(depositFrom, self, actualTokenAmount);
+
+        // increase the corresponding collateral pool
+        CollateralPool.exists(account.trustlessProductIdTrustedByAccount)
+            .increaseCollateralBalance(collateralType, actualTokenAmount);
+
+        // update account liquidator booster balance if necessary 
+        if (liquidationBoosterTopUp > 0) {
+            account.collaterals[collateralType].increaseLiquidationBoosterBalance(liquidationBoosterTopUp);
+            emit Collateral.LiquidatorBoosterUpdate(
+                accountId, collateralType, liquidationBoosterTopUp.toInt(), block.timestamp
             );
         }
 
-        collateralType.safeTransferFrom(depositFrom, self, actualTokenAmount);
-        account.collaterals[collateralType].increaseCollateralBalance(tokenAmount);
-        collateralPool.increaseCollateralBalance(collateralType, tokenAmount);
-        emit Collateral.CollateralUpdate(accountId, collateralType, tokenAmount.toInt(), block.timestamp);
+        // update account collateral balance if necessary 
+        if (tokenAmount > 0) {
+            account.collaterals[collateralType].increaseCollateralBalance(tokenAmount);
+            emit Collateral.CollateralUpdate(accountId, collateralType, tokenAmount.toInt(), block.timestamp);
+        }
 
         emit Deposited(accountId, collateralType, actualTokenAmount, msg.sender, block.timestamp);
     }
