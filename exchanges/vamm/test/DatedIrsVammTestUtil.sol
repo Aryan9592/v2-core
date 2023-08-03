093109747b642bcefa64071efb5143447529ae58
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-08-03 15:16:35 +0200

    feat: paste vamm logic

---
+++ exchanges/vamm/test/DatedIrsVammTestUtil.sol
@@ -0,0 +1,362 @@
+pragma solidity >=0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "./VoltzTest.sol";
+import "../src/storage/LPPosition.sol";
+import "../src/storage/DatedIrsVAMM.sol";
+import "../src/libraries/errors/CustomErrors.sol";
+import "../src/libraries/ticks/Tick.sol";
+
+import "@voltz-protocol/util-contracts/src/helpers/SafeCast.sol";
+
+import { mulUDxInt } from "@voltz-protocol/util-contracts/src/helpers/PrbMathHelper.sol";
+import { UD60x18, convert, ud60x18, uMAX_UD60x18, uUNIT } from "@prb/math/UD60x18.sol";
+import { SD59x18, sd59x18, convert } from "@prb/math/SD59x18.sol";
+
+/// @dev Contains assertions and other functions used by multiple tests
+contract DatedIrsVammTestUtil is VoltzTest {
+    // Contracts under test
+    using DatedIrsVamm for DatedIrsVamm.Data;
+    using SafeCastU256 for uint256;
+    using SafeCastI256 for int256;
+    uint256 internal vammId;
+    address constant mockRateOracle = 0xAa73aA73Aa73Aa73AA73Aa73aA73AA73aa73aa73;
+
+    // Initial VAMM state
+    // Picking a price that lies on a tick boundry simplifies the math to make some tests and checks easier
+    int24 initialTick = -32191;
+    uint160 initSqrtPriceX96 = TickMath.getSqrtRatioAtTick(initialTick); // price = ~0.04 = ~4%
+    uint128 initMarketId = 1;
+    int24 initTickSpacing = 1; // TODO: test with different tick spacing; need to adapt boundTicks()
+    uint32 initMaturityTimestamp = uint32(block.timestamp + convert(FixedAndVariableMath.SECONDS_IN_YEAR));
+    VammConfiguration.Mutable internal mutableConfig = VammConfiguration.Mutable({
+        priceImpactPhi: ud60x18(1e17), // 0.1
+        priceImpactBeta: ud60x18(125e15), // 0.125
+        spread: ud60x18(3e15), // spread / 2 = 0.3%
+        rateOracle: IRateOracle(mockRateOracle),
+        minTick: MIN_TICK,
+        maxTick: MAX_TICK
+    });
+
+    VammConfiguration.Immutable internal immutableConfig = VammConfiguration.Immutable({
+        maturityTimestamp: initMaturityTimestamp,
+        _maxLiquidityPerTick: 1e27,
+        _tickSpacing: 1,
+        marketId: initMarketId
+    });
+
+    /// @notice Computes the amount of liquidity per tick to use for a given base amount and price range
+    /// @dev Calculates `baseAmount / (sqrtRatio(tickUpper) - sqrtRatio(tickLower))`.
+    /// @param tickLower The first tick boundary
+    /// @param tickUpper The second tick boundary
+    /// @param baseAmount The amount of base token being sent in
+    /// @return liquidity The amount of liquidity per tick
+    function getLiquidityForBase(
+        int24 tickLower,
+        int24 tickUpper,
+        int256 baseAmount
+    ) public pure returns (int128 liquidity) {
+
+        // get sqrt ratios
+        uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(tickLower);
+        uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(tickUpper);
+
+        if (sqrtRatioAX96 > sqrtRatioBX96)
+            (sqrtRatioAX96, sqrtRatioBX96) = (sqrtRatioBX96, sqrtRatioAX96);
+        uint256 absLiquidity = FullMath
+                .mulDiv(uint256(baseAmount > 0 ? baseAmount : -baseAmount), VAMMBase.Q96, sqrtRatioBX96 - sqrtRatioAX96);
+
+        return baseAmount > 0 ? absLiquidity.toInt().to128() : -(absLiquidity.toInt().to128());
+    }
+
+    function tickDistanceFromCurrentToTick(ExposedDatedIrsVamm vamm, int24 _tick) public view returns (uint256 absoluteDistance) {
+        int24 currentTick = vamm.tick();
+        return tickDistance(currentTick, _tick);
+    }
+
+    function boundNewPositionLiquidityAmount(
+        ExposedDatedIrsVamm vamm,
+        int24 tickLower,
+        int24 tickUpper,
+        int128 unboundLiquidityDelta)
+    internal view returns (int128 liquidityDelta)
+    {
+        // Ticks must be in range and cannot be equal
+        // uint256 tickRange = tickDistance(_tickLower, _tickUpper);
+        uint128 maxLiquidityPerTick = vamm.maxLiquidityPerTick();
+        //int256 max = min(int256(type(int128).max), int256(uint256(maxLiquidityPerTick)*tickRange));
+        int256 max = min(int256(type(int128).max - 1), int256(uint256(maxLiquidityPerTick))); // TODO: why is type(int128).max not safe?
+
+        // Amounts of liquidty smaller than required for base amount of 100k might produce acceptable rounding errors that nonetheless make tests fiddly
+        int256 min = getLiquidityForBase(tickLower, tickUpper, 1000_000); 
+
+        return int128(bound(unboundLiquidityDelta, min, max)); // New positions cannot withdraw liquidity so >= 0
+    }
+
+    function boundNewPositionLiquidityAmount(
+        uint128 maxLiquidityPerTick,
+        int24 tickLower,
+        int24 tickUpper,
+        int128 unboundLiquidityDelta)
+    internal view returns (int128 liquidityDelta)
+    {
+        //int256 max = min(int256(type(int128).max), int256(uint256(maxLiquidityPerTick)*tickRange));
+        int256 max = min(int256(type(int128).max - 1), int256(uint256(maxLiquidityPerTick))); // TODO: why is type(int128).max not safe?
+
+        // Amounts of liquidty smaller than required for base amount of 100k might produce acceptable rounding errors that nonetheless make tests fiddly
+        int256 min = getLiquidityForBase(tickLower, tickUpper, 1000_000); 
+
+        return int128(bound(unboundLiquidityDelta, min, max)); // New positions cannot withdraw liquidity so >= 0
+    }
+
+    function logTickInfo(string memory label, int24 _tick) view internal {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        console2.log(label);
+        console2.log("- tickNumber", int256(_tick));
+        Tick.Info storage t = vamm.vars._ticks[_tick];
+        console2.log("- liquidityGross", uint256(t.liquidityGross));
+        console2.log("- liquidityNet", int256(t.liquidityNet));
+        console2.log("- trackerQuoteTokenGrowthOutsideX128", t.trackerQuoteTokenGrowthOutsideX128);
+        console2.log("- trackerBaseTokenGrowthOutsideX128", t.trackerBaseTokenGrowthOutsideX128);
+        console2.log("- initialized", t.initialized);
+    }
+}
+
+contract ExposedDatedIrsVamm {
+
+    using DatedIrsVamm for DatedIrsVamm.Data;
+    using PoolConfiguration for PoolConfiguration.Data;
+
+    uint256 vammId;
+
+    constructor(uint256 _vammId) {
+        vammId = _vammId;
+    }
+
+    function create(
+        uint128 _marketId,
+        uint160 _sqrtPriceX96,
+        uint32[] calldata times, 
+        int24[] calldata observedTicks,
+        VammConfiguration.Immutable memory _config,
+        VammConfiguration.Mutable memory _mutableConfig
+    ) external returns (bytes32 s){
+        DatedIrsVamm.Data storage vamm =  DatedIrsVamm.create(
+            _marketId,
+            _sqrtPriceX96,
+            times,
+            observedTicks,
+            _config,
+            _mutableConfig
+        );
+
+        assembly {
+            s := vamm.slot
+        }
+    }
+
+    function configureVamm(
+        VammConfiguration.Mutable memory _mutableConfig
+    ) external {
+        DatedIrsVamm.load(vammId).configure(_mutableConfig);
+    }
+
+    function load(
+    ) public view returns (bytes32 s) {
+        DatedIrsVamm.Data storage vamm =  DatedIrsVamm.load(vammId);
+        assembly {
+            s := vamm.slot
+        }
+    }
+
+    function initialize(uint160 _sqrtPriceX96, uint32[] memory times, int24[] memory observedTicks) public {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        vamm.initialize(_sqrtPriceX96, times, observedTicks);
+    }
+
+    function twap(uint32 secondsAgo, int256 orderSize, bool adjustForPriceImpact,  bool adjustForSpread) 
+        public
+        view
+        returns (UD60x18 geometricMeanPrice) 
+    {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.twap( secondsAgo, orderSize, adjustForPriceImpact, adjustForSpread);
+    }
+
+    function observe(uint32 secondsAgo) public view returns (int24) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.observe(secondsAgo);
+    }
+
+    function observe( uint32[] memory secondsAgo) public view returns (int56[] memory, uint160[] memory) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.observe(secondsAgo);
+    }
+
+     function increaseObservationCardinalityNext( uint16 _observationCardinalityNext) public{
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.increaseObservationCardinalityNext(_observationCardinalityNext);
+    }
+
+    function executeDatedMakerOrder(
+        uint128 accountId,
+        uint128 marketId,
+        int24 tickLower,
+        int24 tickUpper,
+        int128 liquidityDelta
+    ) public {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        vamm.executeDatedMakerOrder(accountId, marketId, tickLower, tickUpper, liquidityDelta);
+    }
+
+    function configure(VammConfiguration.Mutable memory _config) public {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        vamm.configure(_config);
+    }
+
+    function getAccountFilledBalances(uint128 accountId) public view returns (int256, int256) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.getAccountFilledBalances(accountId);
+    }
+
+    function getAccountUnfilledBalances(uint128 accountId) 
+        public view returns (uint256, uint256, uint256, uint256) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.getAccountUnfilledBalances(accountId);
+    }
+
+    function vammSwap(DatedIrsVamm.SwapParams memory params) public returns (int256, int256) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.vammSwap(params);
+    }
+
+    function computeGrowthInside(int24 tickLower, int24 tickUpper) public view returns (int256, int256) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.computeGrowthInside(tickLower, tickUpper);
+    }
+
+    function updatePositionTokenBalances(
+        uint128 accountId,
+        uint128 marketId,
+        uint32 maturityTimestamp,
+        int24 tickLower,
+        int24 tickUpper,
+        bool isMintBurn
+    ) public returns (LPPosition.Data memory) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        LPPosition.Data storage _position = LPPosition.load(
+            LPPosition.getPositionId(accountId, marketId, maturityTimestamp, tickLower, tickUpper)
+        );
+        if (_position.accountId == 0) {
+            _position = LPPosition.create(accountId, marketId, maturityTimestamp, tickLower, tickUpper);
+        }
+        vamm.updatePositionTokenBalances(_position, tickLower, tickUpper, isMintBurn);
+
+        return _position;
+    }
+
+    function getPriceFromTick(int24 _tick) public view returns (UD60x18 price) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.getPriceFromTick(_tick);
+    }
+
+    function baseBetweenTicks(
+        int24 _tickLower,
+        int24 _tickUpper,
+        int128 _liquidityPerTick
+    ) public view returns(int256) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.baseBetweenTicks(_tickLower, _tickUpper, _liquidityPerTick);
+    }
+
+    function unbalancedQuoteBetweenTicks(
+        int24 _tickLower,
+        int24 _tickUpper,
+        int256 baseAmount
+    ) public view returns(int256) {
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.load(vammId);
+        return vamm.unbalancedQuoteBetweenTicks(_tickLower, _tickUpper, baseAmount);
+    }
+
+    ///// HELPERS
+
+    function setMakerPositionsPerAccountLimit(uint256 limit) public {
+        PoolConfiguration.load().setMakerPositionsPerAccountLimit(limit);
+    }
+
+    ///// GETTERS
+
+    function tick() external view returns (int24){
+        return DatedIrsVamm.load(vammId).vars.tick;
+    }
+
+    function liquidity() external view returns (uint128){
+        return DatedIrsVamm.load(vammId).vars.liquidity;
+    }
+
+    function ticks(int24 _tick) external view returns (Tick.Info memory){
+        return DatedIrsVamm.load(vammId).vars._ticks[_tick];
+    }
+
+    function sqrtPriceX96() external view returns (uint160){
+        return DatedIrsVamm.load(vammId).vars.sqrtPriceX96;
+    }
+
+    function observationCardinality() external view returns (uint16){
+        return DatedIrsVamm.load(vammId).vars.observationCardinality;
+    }
+
+    function observationIndex() external view returns (uint16){
+        return DatedIrsVamm.load(vammId).vars.observationIndex;
+    }
+
+    function observationCardinalityNext() external view returns (uint16){
+        return DatedIrsVamm.load(vammId).vars.observationCardinalityNext;
+    }
+
+    function observationAtIndex(uint16 index) external view returns (Oracle.Observation memory) {
+        return DatedIrsVamm.load(vammId).vars.observations[index];
+    } 
+
+    function observations() external view returns (Oracle.Observation[65535] memory) {
+        return DatedIrsVamm.load(vammId).vars.observations;
+    } 
+
+    function unlocked() external view returns (bool){
+        return DatedIrsVamm.load(vammId).vars.unlocked;
+    }
+
+    function priceImpactPhi() external view returns (UD60x18){
+        return DatedIrsVamm.load(vammId).mutableConfig.priceImpactPhi;
+    }
+
+    function priceImpactBeta() external view returns (UD60x18){
+        return DatedIrsVamm.load(vammId).mutableConfig.priceImpactBeta;
+    }
+
+    function spread() external view returns (UD60x18){
+        return DatedIrsVamm.load(vammId).mutableConfig.spread;
+    }
+
+    function rateOracle() external view returns (IRateOracle){
+        return DatedIrsVamm.load(vammId).mutableConfig.rateOracle;
+    }
+
+    function maxLiquidityPerTick() external view returns (uint128){
+        return DatedIrsVamm.load(vammId).immutableConfig._maxLiquidityPerTick;
+    }
+
+    function trackerBaseTokenGrowthGlobalX128() external view returns (int256){
+        return DatedIrsVamm.load(vammId).vars.trackerBaseTokenGrowthGlobalX128;
+    }
+
+    function trackerQuoteTokenGrowthGlobalX128() external view returns (int256){
+        return DatedIrsVamm.load(vammId).vars.trackerQuoteTokenGrowthGlobalX128;
+    }
+
+    function position(uint128 posId) external pure returns (LPPosition.Data memory){
+        return LPPosition.load(posId);
+    }
+
+}
---
+++ exchanges/vamm/test/VoltzTest.sol
@@ -0,0 +1,133 @@
+pragma solidity >=0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "../utils/vamm-math/TickMath.sol";
+import { mulUDxInt } from "@voltz-protocol/util-contracts/src/helpers/PrbMathHelper.sol";
+import { UD60x18, convert, ud60x18, uMAX_UD60x18, uUNIT } from "@prb/math/UD60x18.sol";
+import { SD59x18, sd59x18, convert } from "@prb/math/SD59x18.sol";
+import "@voltz-protocol/util-contracts/src/helpers/SafeCast.sol";
+
+
+/// @dev Contains assertions and other functions used by multiple tests
+contract VoltzTest is Test {
+    using SafeCastU256 for uint256;
+
+    /// @dev The minimum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**-128
+    int24 internal constant MIN_TICK = -69100;
+    /// @dev The maximum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**128
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    // Helpers
+    function abs(int256 x) pure internal returns (uint256) {
+        return x >= 0 ? uint256(x) : uint256(-x);
+    }
+    function min(int256 a, int256 b) pure internal returns (int256) {
+        return a > b ? b : a;
+    }
+    function min(int24 a, int24 b) pure internal returns (int24) {
+        return a > b ? b : a;
+    }
+    // function max(int256 a, int256 b) pure internal returns (int256) {
+    //     return a < b ? b : a;
+    // }
+    function logTicks(int24 a, int24 b, string memory _message) internal view {
+        // string memory message = bytes(_message).length > 0 ? _message : "Ticks: ";
+        console2.log(_message, bytes(_message).length > 0 ? " ticks: " : "Ticks:"); // TODO_delete_log
+        console2.logInt(a);
+        console2.logInt(b);
+    }
+    function boundTicks(
+        int24 _tickLower,
+        int24 _tickUpper)
+    internal view returns (int24 tickLower, int24 tickUpper)
+    {
+        // Ticks must be in range and cannot be equal
+        tickLower = int24(bound(_tickLower, MIN_TICK, MAX_TICK - 1));
+        tickUpper = int24(bound(_tickUpper, MIN_TICK + 1, MAX_TICK));
+        vm.assume(tickLower < tickUpper);
+    }
+    function tickDistance(int24 _tickA, int24 _tickB) public pure returns (uint256 absoluteDistance) {
+        return abs(_tickA - _tickB);
+    }
+    function sqrtPriceDistanceX96(int24 _tickA, int24 _tickB) public pure returns (uint256 absoluteSqrtPriceDistanceX96) {
+        uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(_tickA);
+        uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(_tickB);
+        return abs(uint256(sqrtRatioAX96).toInt() - uint256(sqrtRatioBX96).toInt());
+    }
+
+    using SafeCastI256 for uint256;
+
+    // Assertions
+    function assertEq(UD60x18 a, UD60x18 b) internal {
+        assertEq(UD60x18.unwrap(a), UD60x18.unwrap(b));
+    }
+    function assertEq(SD59x18 a, SD59x18 b) internal {
+        assertEq(SD59x18.unwrap(a), SD59x18.unwrap(b));
+    }
+    function assertGt(UD60x18 a, UD60x18 b) internal {
+        assertGt(UD60x18.unwrap(a), UD60x18.unwrap(b));
+    }
+    function assertLt(UD60x18 a, UD60x18 b) internal {
+        assertLt(UD60x18.unwrap(a), UD60x18.unwrap(b));
+    }
+    function assertAlmostEqual(SD59x18 a, SD59x18 b, SD59x18 deltaAsFractionOfA) internal {
+        if (SD59x18.unwrap(a) == SD59x18.unwrap(b)) {
+            // Equal (needed for case where a = b = 0)
+            return;
+        }
+        SD59x18 upperBound = SD59x18.unwrap(a) >= 0 ? a.add(deltaAsFractionOfA.mul(a)) : a.sub(deltaAsFractionOfA.mul(a));
+        SD59x18 lowerBound = SD59x18.unwrap(a) >= 0 ? a.sub(deltaAsFractionOfA.mul(a)) : a.add(deltaAsFractionOfA.mul(a));
+        if (b.gt(upperBound) || b.lt(lowerBound)) {
+            // console2.log("Expected the following two values to be almost equal:"); // TODO_delete_log
+            console2.logInt(SD59x18.unwrap(a));
+            console2.logInt(SD59x18.unwrap(b));
+        }
+        assertGe(SD59x18.unwrap(b), SD59x18.unwrap(lowerBound) );
+        assertLe(SD59x18.unwrap(b), SD59x18.unwrap(upperBound) );
+    }
+    function assertAlmostEqual(UD60x18 a, UD60x18 b, UD60x18 deltaAsFractionOfA) internal {
+        if (UD60x18.unwrap(a) == UD60x18.unwrap(b)) {
+            // Equal (needed for case where a = b = 0)
+            return;
+        }
+
+        UD60x18 upperBound = a.add(deltaAsFractionOfA.mul(a));
+        UD60x18 lowerBound = a.sub(deltaAsFractionOfA.mul(a));
+        if (b.gt(upperBound) || b.lt(lowerBound)) {
+            console.log("Expected %s <= %s <= %s", UD60x18.unwrap(lowerBound), UD60x18.unwrap(b), UD60x18.unwrap(upperBound));
+        }
+        assertGe(UD60x18.unwrap(b), UD60x18.unwrap(lowerBound) );
+        assertLe(UD60x18.unwrap(b), UD60x18.unwrap(upperBound) );
+    }
+    function assertAlmostEqual(UD60x18 a, UD60x18 b) internal {
+        UD60x18 deltaAsFractionOfA = ud60x18(1e14); // 0.01%
+        assertAlmostEqual(a, b, deltaAsFractionOfA);
+    }
+    function assertAlmostExactlyEqual(UD60x18 a, UD60x18 b) internal {
+        UD60x18 deltaAsFractionOfA = ud60x18(1e12); // 0.0001%
+        assertAlmostEqual(a, b, deltaAsFractionOfA);
+    }
+    function assertAlmostEqual(SD59x18 a, SD59x18 b) internal {
+        SD59x18 deltaAsFractionOfA = sd59x18(1e14); // 0.01%
+        assertAlmostEqual(a, b, deltaAsFractionOfA);
+    }
+    function assertAlmostExactlyEqual(SD59x18 a, SD59x18 b) internal {
+        SD59x18 deltaAsFractionOfA = sd59x18(1e12); // 0.0001%
+        assertAlmostEqual(a, b, deltaAsFractionOfA);
+    }
+    function assertAlmostEqual(int256 a, int256 b) internal {
+        assertAlmostEqual(SD59x18.wrap(a), SD59x18.wrap(b));
+    }
+    function assertAlmostEqual(int256 a, int256 b, uint256 deltaAsFractionOfA) internal {
+        assertAlmostEqual(SD59x18.wrap(a), SD59x18.wrap(b), SD59x18.wrap(deltaAsFractionOfA.toInt()));
+    }
+    function assertOffByNoMoreThan2OrAlmostEqual(int256 a, int256 b) internal {
+        if (abs(a-b) > 2) {
+            assertAlmostEqual(SD59x18.wrap(a), SD59x18.wrap(b));
+        }
+    }
+    function assertEq(UD60x18 a, UD60x18 b, string memory err) internal {
+        assertEq(UD60x18.unwrap(a), UD60x18.unwrap(b), err);
+    }
+}
