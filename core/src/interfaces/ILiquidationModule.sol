92b6af5bdc57670722316b13fd61da57d857eecb
Artur Begyan <arturbegyan98@gmail.com>
2023-09-10 17:54:43 +0400

    feat: updates to the liquidation module comments

--- core/src/interfaces/ILiquidationModule.sol
+++ core/src/interfaces/ILiquidationModule.sol
@@ -15,10 +15,13 @@ import "../storage/Account.sol";
 interface ILiquidationModule {
 
     /**
-     * @notice Get the im and lm requirements and highest unrealized loss along with the flags for im or lm satisfied 
+     * @notice Get the initial and liquidation margin requirements and highest unrealized loss
+     * (if maker positions were to be filled) along with the flags that specify if the initial or liquidation margin
+     * requirements are satisfied.
      * @param accountId The id of the account that is being checked
-     * @param collateralType The collateral type of the account that is being checked
-     * @return Margin requirement information
+     * @param collateralType The collateral type for which the margin requirements are checked,
+     * where the collateral type is the centre of a given collateral bubble
+     * @return Margin requirement information object
      */
     function getRequirementDeltasByBubble(uint128 accountId, address collateralType) 
         external 
