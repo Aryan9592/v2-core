f29ef80311d35ef7bdbd69fa2be87e4b04f804f5
Artur Begyan <arturbegyan98@gmail.com>
2023-09-10 15:39:03 +0400

    feat: auto-exchnage comments update

--- core/src/interfaces/IAutoExchangeModule.sol
+++ core/src/interfaces/IAutoExchangeModule.sol
@@ -11,5 +11,32 @@ pragma solidity >=0.8.19;
  * @title Auto Exchange Module Interface
  */
 interface IAutoExchangeModule {
-    // todo: implement after freezing storages
+
+    // todo: consider returning more information (like we do with liquidations)
+    /**
+     * @notice Checks if an account is eligble for auto-exchange
+     * @param accountId The id of the account that is being checked for auto-exchange eligibility
+     * @param quoteType The quote token's address to be covered in the auto exchange
+     * @return isEligibleForAutoExchange True if the account collateral is liquidatable for a given quote
+     * token
+     */
+    function isEligibleForAutoExchange(uint128 accountId, address quoteType) external view returns (
+        bool isEligibleForAutoExchange
+    );
+
+    /** 
+     * @notice Returns the maximum amount that can be auto-exchanged, represented in quote token terms
+     * @param accountId The id of the account that is being auto-exchanged
+     * @param coveringToken The collateral that is supposed to be used for covering the deficit in a given quote token
+     * @param autoExchangedToken The quote token in deficit that will be compensated by the liquidator as part of
+     * an auto-exchange
+     * @return coveringAmount The amount of `coveringToken` used to cover the  `autoExchangedToken` deficit
+     * @return autoExchangedAmount The maximum amount of `autoExchangedToken` equivalent to the `coveringAmount`
+     * in terms of the `coveringToken`
+    */
+    function getMaxAmountToExchangeQuote(
+        uint128 accountId,
+        address coveringToken,
+        address autoExchangedToken
+    ) external view returns (uint256 coveringAmount, uint256 autoExchangedAmount);
 }
