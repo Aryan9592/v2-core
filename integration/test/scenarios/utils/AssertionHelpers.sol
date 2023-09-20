c550541591b96a8b9a87a7957b0327f9a7a41115
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-09-20 21:34:04 +0200

    feat: simple setup draft

---
+++ integration/test/scenarios/utils/AssertionHelpers.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.8.19;
+
+import "forge-std/Test.sol";
+import "@voltz-protocol/v2-vamm/src/libraries/ticks/TickMath.sol";
+import "@voltz-protocol/v2-vamm/src/libraries/math/FullMath.sol";
+import "@voltz-protocol/v2-vamm/src/libraries/math/FixedPoint96.sol";
+import {Account} from "@voltz-protocol/core/src/storage/Account.sol";
+import {DatedIrsRouter, DatedIrsProxy} from "../../../src/proxies/DatedIrs.sol";
+
+contract AssertionHelpers is Test {
+
+    function assertAlmostEq(int256 a, int256 b, uint256 eps) public {
+        assertGe(a, b - int256(eps));
+        assertLe(a, b + int256(eps));
+    }
+
+    function assertAlmostEq(int256 a, uint256 b, uint256 eps) public {
+        assertGe(a, int256(b - eps));
+        assertLe(a, int256(b + eps));
+    }
+
+    function assertAlmostEq(uint256 a, uint256 b, uint256 eps) public {
+        assertGe(a, b - eps);
+        assertLe(a, b + eps);
+    }
+
+    function absUtil(int256 a) public returns (uint256){
+        return a > 0 ? uint256(a) : uint256(-a);
+    }
+
+    function absOrZero(int256 a) public returns (uint256){
+        return a < 0 ? uint256(-a) : 0;
+    }
+
+    function timeFactor(uint32 maturityTimestamp) public returns (uint256) {
+        return (uint256(maturityTimestamp) - block.timestamp) * 1e18 / (365 * 24 * 60 * 60);
+    } 
+
+    function priceFromTick(int24 _tick) public returns (uint256) {
+        uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(_tick);
+        uint256 priceX96 = FullMath.mulDiv(sqrtPriceX96, sqrtPriceX96, FixedPoint96.Q96);
+        return FullMath.mulDiv(1e18, FixedPoint96.Q96, priceX96);
+    } 
+
+}
---
+++ integration/test/scenarios/utils/Constants.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.8.19;
+
+/// @title Constant state
+/// @notice Constant state used by the Integration tests
+library Constants {
+    uint256 internal constant Q96 = 0x1000000000000000000000000;
+    uint256 internal constant WAD = 1_000_000_000_000_000_000;
+}
