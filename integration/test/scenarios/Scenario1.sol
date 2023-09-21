f9635dd3afcf01b894d19a59ac0d8edf53c1a294
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-09-21 13:01:02 +0200

    feat: draft action-checks

--- integration/test/scenarios/Scenario1.sol
+++ integration/test/scenarios/Scenario1.sol
@@ -14,103 +14,134 @@ import {VammConfiguration} from "@voltz-protocol/v2-vamm/src/libraries/vamm-util
 import {DatedIrsVamm} from "@voltz-protocol/v2-vamm/src/storage/DatedIrsVamm.sol";
 import {TickMath} from "@voltz-protocol/v2-vamm/src/libraries/ticks/TickMath.sol";
 import { IERC20 } from "oz/interfaces/IERC20.sol";
+import {Actions} from "./utils/Actions.sol";
+import {Checks} from "./utils/Checks.sol";
 
 import { ud60x18, div, SD59x18, UD60x18 } from "@prb/math/UD60x18.sol";
 
-contract AtomicScenarios is ScenarioSetup, AssertionHelpers {
-  using SafeCastI256 for int256;
-  using SafeCastU256 for uint256;
-  using SafeCastU128 for uint128;
+contract Scenario1 is ScenarioSetup, AssertionHelpers, Actions, Checks {
+    using SafeCastI256 for int256;
+    using SafeCastU256 for uint256;
+    using SafeCastU128 for uint128;
 
-  address internal user1;
-  address internal user2;
+    address internal user1;
+    address internal user2;
 
-  uint128 productId;
-  uint128 marketId;
-  uint32 maturityTimestamp;
-  int24 initTick;
+    uint128 productId;
+    uint128 marketId;
+    uint32 maturityTimestamp;
+    int24 initTick;
 
-  using SetUtil for SetUtil.Bytes32Set;
+    using SetUtil for SetUtil.Bytes32Set;
 
-  function setUp() public {
-    super.datedIrsSetup();
-    user1 = vm.addr(1);
-    user2 = vm.addr(2);
-    marketId = 1;
-    maturityTimestamp = uint32(block.timestamp) + 365 * 86400; // in 4 days
-    initTick = -13860; // 4%
-  }
+    function setUp() public {
+        super.datedIrsSetup();
+        user1 = vm.addr(1);
+        user2 = vm.addr(2);
+        marketId = 1;
+        maturityTimestamp = uint32(block.timestamp) + 365 * 86400; // in 4 days
+        initTick = -13860; // 4%
+    }
 
-  function setConfigs() public {
-    vm.startPrank(owner);
+    function setConfigs() public {
+        vm.startPrank(owner);
 
-    //////// MARKET MANAGER CONFIGURATION ////////
+        //////// MARKET MANAGER CONFIGURATION ////////
 
-    datedIrsProxy.createMarket({
-        marketId: marketId,
-        quoteToken: address(mockToken),
-        marketType: "compounding"
-    });
-    datedIrsProxy.setMarketConfiguration(
-        marketId,
-        Market.MarketConfiguration({
-            poolAddress: address(vammProxy),
-            twapLookbackWindow: 7 * 86400, // 7 days
-            markPriceBand: ud60x18(1e17), // 10%
-            takerPositionsPerAccountLimit: 100,
-            positionSizeUpperLimit: 1e27, // 1B
-            positionSizeLowerLimit: 0,
-            openInterestUpperLimit: 1e27 // 1B
-        })
-    );
-    datedIrsProxy.setRateOracleConfiguration(
-        marketId,
-        Market.RateOracleConfiguration({
-            oracleAddress: address(aaveV3RateOracle),
-            maturityIndexCachingWindowInSeconds: 1e27 // 1B
-        })
-    );
+        datedIrsProxy.createMarket({
+            marketId: marketId,
+            quoteToken: address(mockToken),
+            marketType: "compounding"
+        });
+        datedIrsProxy.setMarketConfiguration(
+            marketId,
+            Market.MarketConfiguration({
+                poolAddress: address(vammProxy),
+                twapLookbackWindow: 7 * 86400, // 7 days
+                markPriceBand: ud60x18(1e17), // 10%
+                takerPositionsPerAccountLimit: 100,
+                positionSizeUpperLimit: 1e27, // 1B
+                positionSizeLowerLimit: 0,
+                openInterestUpperLimit: 1e27 // 1B
+            })
+        );
+        datedIrsProxy.setRateOracleConfiguration(
+            marketId,
+            Market.RateOracleConfiguration({
+                oracleAddress: address(aaveV3RateOracle),
+                maturityIndexCachingWindowInSeconds: 1e27 // 1B
+            })
+        );
 
-    //////// VAMM CONFIGURATION ////////
+        //////// VAMM CONFIGURATION ////////
 
-    vammProxy.setPoolConfiguration(PoolConfiguration.Data({
-        marketManagerAddress: address(datedIrsProxy),
-        makerPositionsPerAccountLimit: 1e27 // 1B
-    }));
+        vammProxy.setPoolConfiguration(PoolConfiguration.Data({
+            marketManagerAddress: address(datedIrsProxy),
+            makerPositionsPerAccountLimit: 1e27 // 1B
+        }));
 
-    DatedIrsVamm.Immutable memory immutableConfig = DatedIrsVamm.Immutable({
-        maturityTimestamp: maturityTimestamp,
-        maxLiquidityPerTick: type(uint128).max,
-        tickSpacing: 60,
-        marketId: marketId
-    });
+        DatedIrsVamm.Immutable memory immutableConfig = DatedIrsVamm.Immutable({
+            maturityTimestamp: maturityTimestamp,
+            maxLiquidityPerTick: type(uint128).max,
+            tickSpacing: 60,
+            marketId: marketId
+        });
 
-    DatedIrsVamm.Mutable memory mutableConfig = DatedIrsVamm.Mutable({
-        priceImpactPhi: ud60x18(1e18), // 1
-        spread: ud60x18(0), // 0%
-        minSecondsBetweenOracleObservations: 10,
-        minTickAllowed: TickMath.DEFAULT_MIN_TICK,
-        maxTickAllowed: TickMath.DEFAULT_MAX_TICK
-    });
+        DatedIrsVamm.Mutable memory mutableConfig = DatedIrsVamm.Mutable({
+            priceImpactPhi: ud60x18(1e18), // 1
+            spread: ud60x18(0), // 0%
+            minSecondsBetweenOracleObservations: 10,
+            minTickAllowed: TickMath.DEFAULT_MIN_TICK,
+            maxTickAllowed: TickMath.DEFAULT_MAX_TICK
+        });
 
-    // ensure the current time > 1st day
-    uint32[] memory times = new uint32[](2);
-    times[0] = uint32(block.timestamp - 86400);
-    times[1] = uint32(block.timestamp - 43200);
-    int24[] memory observedTicks = new int24[](2);
-    observedTicks[0] = -13860;
-    observedTicks[1] = -13860;
-    vammProxy.createVamm({
-        sqrtPriceX96: TickMath.getSqrtRatioAtTick(initTick),
-        times: times,
-        observedTicks: observedTicks,
-        config: immutableConfig,
-        mutableConfig: mutableConfig
-    });
-    vammProxy.increaseObservationCardinalityNext(marketId, maturityTimestamp, 16);
+        // ensure the current time > 1st day
+        uint32[] memory times = new uint32[](2);
+        times[0] = uint32(block.timestamp - 86400);
+        times[1] = uint32(block.timestamp - 43200);
+        int24[] memory observedTicks = new int24[](2);
+        observedTicks[0] = -13860;
+        observedTicks[1] = -13860;
+        vammProxy.createVamm({
+            sqrtPriceX96: TickMath.getSqrtRatioAtTick(initTick),
+            times: times,
+            observedTicks: observedTicks,
+            config: immutableConfig,
+            mutableConfig: mutableConfig
+        });
+        vammProxy.increaseObservationCardinalityNext(marketId, maturityTimestamp, 16);
 
-    vm.stopPrank();
+        vm.stopPrank();
 
-    aaveLendingPool.setReserveNormalizedIncome(IERC20(mockToken), ud60x18(1e18));
-  }
+        aaveLendingPool.setReserveNormalizedIncome(IERC20(mockToken), ud60x18(1e18));
+    }
+
+    function test_MINT() public {
+
+        // action 
+        executeDatedIrsMakerOrder({
+            marketId: marketId,
+            maturityTimestamp: maturityTimestamp,
+            accountId: 1,
+            user: user1,
+            baseAmount: 100,
+            tickLower: 0,
+            tickUpper: 60
+        }); 
+
+        checkUnfilledBalances(
+            address(vammProxy),
+            PositionInfo({accountId: 1, marketId: marketId, maturityTimestamp: maturityTimestamp}),
+            CheckedValueU256({value: 100, toCheck: true}),
+            CheckedValueU256({value: 0, toCheck: false}),
+            CheckedValueU256({value: 100, toCheck: true}),
+            CheckedValueU256({value: 0, toCheck: false})
+        );
+
+
+    }
+
+    function datedIrsProxy() internal returns (DatedIrsProxy) {
+        return datedIrsProxy;
+    }
 }
--- integration/test/scenarios/utils/AssertionHelpers.sol
+++ integration/test/scenarios/utils/AssertionHelpers.sol
@@ -1,11 +1,11 @@
 pragma solidity >=0.8.19;
 
 import "forge-std/Test.sol";
-import "@voltz-protocol/v2-vamm/src/libraries/ticks/TickMath.sol";
-import "@voltz-protocol/v2-vamm/src/libraries/math/FullMath.sol";
-import "@voltz-protocol/v2-vamm/src/libraries/math/FixedPoint96.sol";
-import {Account} from "@voltz-protocol/core/src/storage/Account.sol";
-import {DatedIrsRouter, DatedIrsProxy} from "../../../src/proxies/DatedIrs.sol";
+import "forge-std/console2.sol";
+
+import {TickMath} from "@voltz-protocol/v2-vamm/src/libraries/ticks/TickMath.sol";
+import {FullMath} from "@voltz-protocol/v2-vamm/src/libraries/math/FullMath.sol";
+import {FixedPoint96} from "@voltz-protocol/v2-vamm/src/libraries/math/FixedPoint96.sol";
 
 contract AssertionHelpers is Test {
 
@@ -22,6 +22,21 @@ contract AssertionHelpers is Test {
     function assertAlmostEq(uint256 a, uint256 b, uint256 eps) public {
         assertGe(a, b - eps);
         assertLe(a, b + eps);
+    }
+
+    function assertAlmostEq(int256 a, int256 b, uint256 eps, string memory message) public {
+        assertGe(a, b - int256(eps), string.concat(message,"_Ge"));
+        assertLe(a, b + int256(eps), string.concat(message,"_Le"));
+    }
+
+    function assertAlmostEq(int256 a, uint256 b, uint256 eps, string memory message) public {
+        assertGe(a, int256(b - eps), string.concat(message,"_Ge"));
+        assertLe(a, int256(b + eps), string.concat(message,"_Le"));
+    }
+
+    function assertAlmostEq(uint256 a, uint256 b, uint256 eps, string memory message) public {
+        assertGe(a, b - eps, string.concat(message,"_Ge"));
+        assertLe(a, b + eps, string.concat(message,"_Le"));
     }
 
     function absUtil(int256 a) public returns (uint256){
