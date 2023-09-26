ad3eed802f113825024d6e3f62380fe1fc386efd
Artur Begyan <arturbegyan98@gmail.com>
2023-09-27 00:55:00 +0300

    ae eligiblility check in trigger flow

--- core/src/libraries/actions/AutoExchange.sol
+++ core/src/libraries/actions/AutoExchange.sol
@@ -7,10 +7,18 @@ https://github.com/Voltz-Protocol/v2-core/blob/main/core/LICENSE
 */
 pragma solidity >=0.8.19;
 
+import {Account} from "../../storage/Account.sol";
+import {AccountAutoExchange} from "../account/AccountAutoExchange.sol";
+
 /**
- * @title Library for trigger auto-exchange
+ * @title Library to trigger auto-exchange
  */
 library AutoExchange {
+
+    using AccountAutoExchange for Account.Data;
+
+    error AccountNotEligibleForAutoExchange(uint128 accountId, address quoteType);
+
     function triggerAutoExchange(
         uint128 accountId,
         uint128 liquidatorAccountId,
@@ -18,6 +26,16 @@ library AutoExchange {
         address collateralType,
         address quoteType
     ) internal {
+
+        // grab the account and check its existance
+        Account.Data storage account = Account.exists(accountId);
+
+        if (!account.isEligibleForAutoExchange(quoteType)) {
+            revert AccountNotEligibleForAutoExchange(accountId, quoteType);
+        }
+
+
+
 
     }
 }
